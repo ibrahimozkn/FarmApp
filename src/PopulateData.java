96c2541f890c39ab33ac98165a1ecc42cec970c6
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2022-10-29 22:14:52 +0300

    PopulateData

--- src/PopulateData.java
+++ src/PopulateData.java
@@ -8,6 +8,12 @@ public class PopulateData {
         cows.add(new Cow(2, "female", LocalDate.of(2001, 1, 5), false));
         cows.add(new Cow(3, "male", LocalDate.of(2013, 9, 21), true));
 
-        return new FarmMe(cows);
+        ArrayList<Veterinary> vets = new ArrayList<Veterinary>();
+
+        vets.add(new Veterinary(1, "female", LocalDate.of(1997, 5, 7), 2500.0));
+        vets.add(new Veterinary(1, "male", LocalDate.of(1998, 12, 27), 2700.0));
+        vets.add(new Veterinary(1, "female", LocalDate.of(1999, 8, 20), 1500.0));
+
+        return new FarmMe(cows, vets);
     }
 }
